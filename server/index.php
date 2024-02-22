c9ff3e83177a882b54918d163b14bbb15bddb72f
Gleb417 <46068523+Gleb417@users.noreply.github.com>
2024-02-22 10:24:28 +0400

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,29 +1,26 @@
 <?php
-
 header('Content-type: application/json');
+$response = [];
+//print_r($_REQUEST);
+if (isset($_REQUEST['method'])) {
+    $method = $_REQUEST['method'];
 
-$method = $_REQUEST['method'];
+    if (!empty($method)) {
+        if (@is_file('methods/'.$method.'.php')) {
+            include 'methods/'.$method.'.php';
 
-if(!empty($method)) {
-
-    if(@is_file('methods/'.$method.'.php')) {
-
-        include 'methods/'.$method.'.php';
-
+        } else {
+            $response['error'] = 'Метод не найден';
+        }
+    } else {
+        $response['error'] = 'Метод не указан';
     }
-
-}
-
-else {
-
-    $response = [
-
-        'error' => 'Метод не найден'
-
-    ];
-
+} else {
+    $response['error'] = 'Метод не указан';
 }
 
 $response = json_encode($response);
-
 echo $response;
+?>
+
+
