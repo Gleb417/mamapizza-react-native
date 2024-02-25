aa968f51ca740470cea58083e04ab950c27fa7ce
Gleb417 <46068523+Gleb417@users.noreply.github.com>
2024-02-25 14:27:34 +0400

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,26 +1,50 @@
 <?php
 header('Content-type: application/json');
-$response = [];
-//print_r($_REQUEST);
-if (isset($_REQUEST['method'])) {
-    $method = $_REQUEST['method'];
 
-    if (!empty($method)) {
-        if (@is_file('methods/'.$method.'.php')) {
-            include 'methods/'.$method.'.php';
+header('Access-Control-Allow-Origin: *');
 
-        } else {
-            $response['error'] = 'Метод не найден';
-        }
-    } else {
-        $response['error'] = 'Метод не указан';
-    }
-} else {
-    $response['error'] = 'Метод не указан';
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     $method = isset($_POST['method']) ? $_POST['method'] : '';
+
+//     if (!empty($method)) {
+//         $methodFilePath = 'methods/'.$method.'.php';
+//         if (is_file($methodFilePath)) {
+//             ob_start(); // Захват вывода
+//             include $methodFilePath;
+//             $response = json_decode(ob_get_contents()); // Получаем данные из подключенного скрипта
+//             ob_end_clean(); // Очистка буфера вывода
+//         } else {
+//             $response['error'] = 'Метод не найден';
+//         }
+//     } else {
+//         $response['error'] = 'Метод не указан';
+//     }
+// } else {
+//     $response['error'] = 'Метод не указан';
+// }
+
+// echo json_encode($response);
+
+$mysqli = new mysqli("192.168.0.103", "root", "", "BDReactNativeMamaPizza");
+
+if ($mysqli->connect_error) {
+    die("Ошибка подключения: " . $mysqli->connect_error);
 }
 
-$response = json_encode($response);
-echo $response;
-?>
+$result = $mysqli->query("SELECT imagepizza, namepizza, descriptionpizza, pricepizza FROM pizza");
 
+if ($result->num_rows > 0) {
+    $data = [];
+    
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    
+    $response = $data;
+    
+    $mysqli->close();
+} else {
+    $mysqli->close();
+}
 
+echo json_encode($response);
